b6eaeab5c7fb89ab2144af9f192bbfb278248d20
Redball45 <jamescann7654@gmail.com>
2017-05-15 21:04:15 +0100

    autorename orun

--- misc/misc.py
+++ misc/misc.py
@@ -14,6 +14,19 @@ class misc:
 	async def summon(self):
 		await self.bot.say("Who dares summon me?")
 
+def rename_orun:
+	orun_id = 202429404503212034
+	user = await self.bot.get_user_info(orun_id)
+	nickname = "Orun"
+	try:
+		await self.bot.change_nickname(user, nickname)
+	except discord.Forbidden:
+		print("I cannot do that, I lack the "
+							   "\"Manage Nicknames\" permission.")
+	await asyncio.sleep(30)
+
 def setup(bot):
-		n = misc(bot)
+	n = misc(bot)
+	loop = asyncio.get_event_loop()
+	loop.create_task(n.rename_orun())
 		bot.add_cog(n)
